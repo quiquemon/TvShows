de994d561df5c1a275bd587ae28722c55f28600d
PROGRAMACION2 <PROGRAMACION2@LAPTOP-PT7LJ4TU>
2017-06-09 12:25:32 -0500

    Dependencies installed. General app settings configured. Layout page and Home Controller created.

---
+++ TvShows/TvShows/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Optimization;
+
+namespace TvShows
+{
+	public class BundleConfig
+	{
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new ScriptBundle("~/app-scripts").Include(
+				"~/Scripts/jquery-3.1.1.min.js",
+				"~/Scripts/bootstrap.min.js"
+			));
+
+			bundles.Add(new StyleBundle("~/app-styles").Include(
+				"~/Content/bootstrap.min.css"
+			));
+		}
+	}
+}
--- TvShows/TvShows/Global.asax.cs
+++ TvShows/TvShows/Global.asax.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
 
 namespace TvShows
 {
@@ -11,8 +8,9 @@ namespace TvShows
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+			AreaRegistration.RegisterAllAreas();
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
